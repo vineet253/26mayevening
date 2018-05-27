7da7ec0223992c3f07b92c68f9dba397e52fac3c
vineet <vineetsharma253@gmail.com>
2018-05-27 16:59:13 +0530

    FIRT COMMIT

---
+++ example.js
@@ -0,0 +1,22 @@
+var a;
+a=10;
+console.log(a);
+
+
+for(let abc=1; abc<100;abc++){
+
+console.log(abc)    
+}
+
+//const constantvariable = 3.14
+
+//constantvariable=123
+
+
+var functionDef = function(){
+console.log('SOME FUNCTION')
+
+
+}
+console.log(functionDef)
+functionDef();
